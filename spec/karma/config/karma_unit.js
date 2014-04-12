8dcbc1f664c8efea283e2bfe905e16dfcc8c98c6
Steve <steve.hook@gmail.com>
2014-04-12 15:47:34 +0100

    Adds the karma-ng-html2js-preprocessor to fix client side tests
    This converts template files, e.g. index.html -> index.html.js so that
    templates are available in specs.

--- spec/karma/config/karma_unit.js
+++ spec/karma/config/karma_unit.js
@@ -27,6 +27,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      '**/*.html': ['ng-html2js']
     },
 
 
@@ -62,6 +63,11 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false
+    singleRun: false,
+
+    ngHtml2JsPreprocessor: {
+      // we want all templates to be loaded in teh same module called 'templates'
+      moduleName: 'templates'
+    }
   });
 };
